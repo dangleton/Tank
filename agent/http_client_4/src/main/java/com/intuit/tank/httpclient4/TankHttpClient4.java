b83ea8359b5911030f49bc4b1db48e721f03f305
Angleton, Denis <denisa@qualcomm.com>
2016-07-11 09:34:20 -0700

    added redirect check back in

--- agent/http_client_4/src/main/java/com/intuit/tank/httpclient4/TankHttpClient4.java
+++ agent/http_client_4/src/main/java/com/intuit/tank/httpclient4/TankHttpClient4.java
@@ -36,9 +36,13 @@ import org.apache.commons.lang.math.NumberUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.NTCredentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.RedirectStrategy;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
@@ -56,9 +60,11 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.BasicCookieStore;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
 import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.protocol.HttpContext;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.apache.log4j.Logger;
 
@@ -99,7 +105,7 @@ public class TankHttpClient4 implements TankHttpClient {
             LOG.error("Error setting accept all: " + e, e);
         }
 
-        httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).setRedirectStrategy(getRedirectStrategy()).build();
         requestConfig = RequestConfig.custom().setSocketTimeout(30000).setConnectTimeout(30000).setCircularRedirectsAllowed(true).setAuthenticationEnabled(true).setRedirectsEnabled(true)
                 .setMaxRedirects(100).build();
         
@@ -111,26 +117,26 @@ public class TankHttpClient4 implements TankHttpClient {
         context.setRequestConfig(requestConfig);
     }
 
-//    private RedirectStrategy getRedirectStrategy() {
-//        RedirectStrategy ret = new DefaultRedirectStrategy() {                
-//            public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context)  {
-//                boolean isRedirect=false;
-//                try {
-//                    isRedirect = super.isRedirected(request, response, context);
-//                } catch (ProtocolException e) {
-//                    LOG.error("Error in protocol: " + e, e);
-//                }
-//                if (!isRedirect) {
-//                    int responseCode = response.getStatusLine().getStatusCode();
-//                    if (responseCode == 301 || responseCode == 302) {
-//                        return true;
-//                    }
-//                }
-//                return isRedirect;
-//            }
-//        };
-//        return ret;
-//    }
+    private RedirectStrategy getRedirectStrategy() {
+        RedirectStrategy ret = new DefaultRedirectStrategy() {                
+            public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context)  {
+                boolean isRedirect=false;
+                try {
+                    isRedirect = super.isRedirected(request, response, context);
+                } catch (ProtocolException e) {
+                    LOG.error("Error in protocol: " + e, e);
+                }
+                if (!isRedirect) {
+                    int responseCode = response.getStatusLine().getStatusCode();
+                    if (responseCode == 301 || responseCode == 302) {
+                        return true;
+                    }
+                }
+                return isRedirect;
+            }
+        };
+        return ret;
+    }
 
     public void setConnectionTimeout(long connectionTimeout) {
         requestConfig = RequestConfig.custom().setSocketTimeout(30000).setConnectTimeout((int) connectionTimeout).setCircularRedirectsAllowed(true).setAuthenticationEnabled(true)
@@ -258,10 +264,10 @@ public class TankHttpClient4 implements TankHttpClient {
         if (StringUtils.isNotBlank(proxyhost)) {
             HttpHost proxy = new HttpHost(proxyhost, proxyport);
             DefaultProxyRoutePlanner routePlanner = new DefaultProxyRoutePlanner(proxy);
-            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).setRoutePlanner(routePlanner).build();
+            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).setRedirectStrategy(getRedirectStrategy()).setRoutePlanner(routePlanner).build();
         } else {
 
-            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+            httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).setRedirectStrategy(getRedirectStrategy()).build();
         }
     }
 
