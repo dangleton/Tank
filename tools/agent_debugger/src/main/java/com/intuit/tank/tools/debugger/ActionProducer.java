429436a0f01dd24b21141a3cfc896c099cc26bef
denisa <denisa@qualcomm.com>
2018-04-09 10:14:16 -0700

    add security back in

--- tools/agent_debugger/src/main/java/com/intuit/tank/tools/debugger/ActionProducer.java
+++ tools/agent_debugger/src/main/java/com/intuit/tank/tools/debugger/ActionProducer.java
@@ -32,16 +32,22 @@ import java.util.Map;
 import java.util.Properties;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.SSLException;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
+import javax.ws.rs.ProcessingException;
 import javax.xml.bind.JAXBException;
 import javax.xml.parsers.ParserConfigurationException;
 
@@ -155,18 +161,22 @@ public class ActionProducer {
         return dataFileClient;
     }
 
-    /**
-     * 
-     * @param serviceUrl
-     */
-    public void setServiceUrl(String serviceUrl) {
+
+    public void setServiceUrl(String serviceUrl, String user, char[] pass) {
         this.scriptServiceClient = new ScriptServiceClient(serviceUrl);
         this.projectServiceClient = new ProjectServiceClientV1(serviceUrl);
         this.dataFileClient = new DataFileClient(serviceUrl);
         this.agentServiceClient = new AgentServiceClient(serviceUrl);
+        if (StringUtils.isNotBlank(user)) {
+            this.scriptServiceClient.addAuth(user, new String(pass).trim());
+            this.projectServiceClient.addAuth(user, new String(pass).trim());
+            this.dataFileClient.addAuth(user, new String(pass).trim());
+            this.agentServiceClient.addAuth(user, new String(pass).trim());
+        }
         PanelBuilder.updateServiceUrl(serviceUrl);
         setChoiceComboBoxOptions(debuggerFrame.getTankClientChooser());
     }
+
 
     /**
      * 
@@ -384,13 +394,26 @@ public class ActionProducer {
             ret = new AbstractAction(ACTION_SELECT_TANK) {
                 private static final long serialVersionUID = 1L;
                 final JComboBox<String> cb = getComboBox();
+                final JTextField userTF = new JTextField();
+
+                final JPasswordField passTF = new JPasswordField();
+                final JComponent[] inputs = new JComponent[] {
+                        new JLabel("URL: "),
+                        cb,
+                        new JLabel("User: "),
+                        userTF,
+                        new JLabel("Pass: "),
+                        passTF
+                };
+
 
                 @Override
                 public void actionPerformed(ActionEvent event) {
                     try {
-                        int selected = JOptionPane.showConfirmDialog(debuggerFrame, cb,
+                        int selected = JOptionPane.showConfirmDialog(debuggerFrame, inputs,
                                 "Enter the base URL to Tank:", JOptionPane.OK_CANCEL_OPTION,
                                 JOptionPane.QUESTION_MESSAGE);
+
                         if (selected == JOptionPane.OK_OPTION) {
                             String url = (String) cb.getSelectedItem();
                             if (url != null) {
@@ -404,9 +427,25 @@ public class ActionProducer {
                                     url = "http://" + url;
                                 }
                                 try {
-                                    new ScriptServiceClient(url).ping();
-                                    setServiceUrl(url);
-                                } catch (Exception e) {
+                                    ScriptServiceClient ssc = new ScriptServiceClient(url);
+                                    if (StringUtils.isNotBlank(userTF.getText())) {
+                                        ssc.addAuth(userTF.getText().trim(), new String(passTF.getPassword()).trim());
+                                    }
+                                    ssc.ping();
+                                    setServiceUrl(url, userTF.getText().trim(),
+                                            passTF.getPassword());
+                                } catch (ProcessingException e) {
+                                    LOG.error("Error making call: " + e, e);
+                                    if (e.getCause() instanceof SSLException) {
+                                        showError("SSl Exception: " + e.toString()
+                                                + ". This could be because of invalid certificates. Please see logs for more detail.");
+
+                                    } else {
+                                        showError("Exception: " + e.toString()
+                                                + ". Please see logs for more detail.");
+                                    }
+                                } catch (RuntimeException e) {
+                                    LOG.error("Error making call: " + e, e);
                                     showError("Cannot connect to Tank at the url " + url
                                             + ". \nExample: http://tank.mysite.com/");
                                 }
